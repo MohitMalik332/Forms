fb584b249ac2730be39f4a46facc36021f725498
hitman <hitman>
2024-11-02 01:21:14 +0530

    Validating input on every key stroke and upon lost focus (Blur)

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -10,6 +10,14 @@ export default function Login() {
     email: '',
     password: '',
   })
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false
+  })
+
+
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
 
 
   function handleSubmit(event) {
@@ -28,6 +36,18 @@ export default function Login() {
       ...prevValues,
       [identifier]: event.target.value //[] this sybol is used to dynamically access a property in an object.
     }));
+
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }))
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: true,
+    }))
   }
 
 
@@ -41,10 +61,12 @@ export default function Login() {
           <input 
             id="email" 
             type="email" 
-            name="email" 
+            name="email"
+            onBlur={() => handleInputBlur('email')}
             onChange={(event) => handleInputChange('email', event)}
             value={enteredValues.email}
           />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
